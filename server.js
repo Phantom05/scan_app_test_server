e81b10c68376d1db39941ab66c41d8bfe6c0c673
phantom05 <monster2jy@gmail.com>
2019-11-01 18:57:34 +0900

    스캔앱 테스트 서버업데이트

--- server.js
+++ server.js
@@ -32,7 +32,7 @@
 // });
 
 
-
+const uuid4 = require('uuid/v4')
 var WebSocketServer = require("ws").Server;
 var port = 8082;
 var wss = new WebSocketServer({ port: port },()=>{
@@ -46,58 +46,118 @@ var wss = new WebSocketServer({ port: port },()=>{
 wss.on("connection", function(ws) {
   console.log(`Socket on ${port}`);
   var emit = (data) => {
-    console.log(data,'sendData');
+    console.log('Server Send : ', data);
 
     const SEND_FORMAT = {"DOF-RES": []};
     SEND_FORMAT['DOF-RES'].push(data);
-    ws.send(JSON.stringify(SEND_FORMAT))
+    try{
+      ws.send(JSON.stringify(SEND_FORMAT))
+    }catch(e){
+      console.log(e.message);
+    }
   };
-
+  
+  
   var parseMessage =(message)=>{
-    return message['DOF-REQ'][0]['code']
+    console.log('\x1b[31m%s\x1b[0m', 'Received Message'); 
+    if(message['DOF-REQ']){
+      console.log('\x1b[36m%s\x1b[0m', JSON.stringify(message));
+      return message['DOF-REQ'][0]['code']
+    }else{
+      console.log('\x1b[33m%s\x1b[0m', JSON.stringify(message)); 
+      return message['DOF-RES'][0]['code']
+    }
   }
-  emit( {"code": ["0000", true] });
+
+  emit( {"code": [["0000", true]] });
+
+  // setTimeout(() => {
+  //   emit( {"code": [["0007", 1]] });
+  // }, 1000);
+  // setTimeout(() => {
+  //   emit( {"code": [["0007", 0]] });
+  // }, 2000);
 
   ws.on("message", function(message) {
-    console.log("Received:");
-    console.log(message);
     message = JSON.parse(message);
     let receive = parseMessage(message);
     let keyName;
     let value;
     // const [keyName,value] = [receive[0],receive[1]];
-
-    if(Array.isArray(receive[0])){
+    // receive.push(['0007',true])
+    if(receive.length >1 ){
+      console.log('다중');
       receive.map((list,idx)=>{
+        let responseMessage = message['DOF-REQ'][0]['code'][idx];
         let resData = 0
         keyName = list[0];
-        if(keyName === '0006'){
-          resData = 4
+        if(list[1] === undefined){
+          //여러개 보냈는데 이름만 확인할때
+          if(keyName === '0006'){
+            resData = 4
+          }
+          if(keyName === '0008'){
+            resData = 0
+          }
+          responseMessage.push(resData);
         }
-        if(keyName === '0008'){
-          resData = 0
+
+        switch(keyName){
+
+          default:{
+
+          }
         }
-        message['DOF-REQ'][0]['code'][idx].push(resData)
       });
+
       emit({"code": parseMessage(message)})
 
     }else{
-      keyName = receive[0]
+      console.log('하나');
+      keyName = receive[0][0];
+      console.log(keyName,'keyName ');
       switch(keyName){
         case "0008" :{
-          emit({"code": [["0008",0]]})
+          console.log('inin');
+          emit({"code": [["0008",true]]});
           break;
         }
         case "0006":{
+          // window
           if(typeof value === 'number' ){
             emit({"code": [["0006",true]]})
           }else{
-            emit({"code": [["0006"]]})
+            emit({"code": [["0006",true]]})
           }
           break;
         }
         case "0403":{
-          emit({"code": [["0403",true]]})
+          // exit
+          emit({"code": [["0403",true]]});
+          break;
+        }
+        case "0402":{
+          // setting
+          emit({"code": [["0402",true]]});
+          break;
+        }
+        case "0400":{
+          let uuidValue = uuid4();
+          console.log(receive);
+          if(receive[0][2] === 0){
+            //create
+            uuidValue = `{${uuidValue}}`;
+            emit({"code": [["0400",uuidValue,true]]});
+          }
+          if(receive[0][2] === 1){
+            //view
+            uuidValue = `{${uuidValue}}`;
+            emit({"code": [["0400",uuidValue,true]]});
+          }
+          break;
+        }
+        case "0007":{
+
           break;
         }
         default:{
@@ -107,9 +167,6 @@ wss.on("connection", function(ws) {
     }
 
 
-
-
-    
   });
 
 });
